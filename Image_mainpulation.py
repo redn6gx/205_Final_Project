dcbf6f1c578d92df66c78abca0ca6157e2e452e8
Ctodd-CSUMB <ctodd@csumb.edu>
2021-05-12 15:53:41 -0700

    Update Image_mainpulation.py
    have all of the options working and displaying the image after being modified

--- Image_mainpulation.py
+++ Image_mainpulation.py
@@ -13,7 +13,7 @@ import sys
 from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QDialog, QTextBrowser, QComboBox, QLineEdit
 from PySide6.QtCore import Qt, Slot
 from PySide6.QtGui import QPixmap
-from PIL import Image
+from PIL import Image, ImageDraw
 import requests, json
 from pprint import pprint
 
@@ -27,7 +27,7 @@ class ImageManipulation(QWidget):
         self.edit_label = QLabel('Change your image')
 
         # set up list and combo box option
-        self.my_list = ["Pick a value", "Sepia", "Negative", "Grayscale", "Thumbnail", "None"]
+        self.my_list = ["Pick a value", "Luminosity", "Contrast", "Colorize", "Sepia", "Negative", "Grayscale", "None"]
         self.my_combo_box = QComboBox()
         self.my_combo_box.addItems(self.my_list)
 
@@ -48,33 +48,81 @@ class ImageManipulation(QWidget):
 
     @Slot()
     def on_edit(self):
-        # take image that was clicked
-        # use your_search as text from the lineedit and make sure it's lowercase
-        # your_search = self.my_lineedit.text()
-        # your_search = your_search.lower()
-        # get_count_from_search(new_dict, your_search)
-
-        # # finds the max occurance and stores it as the max_val
-        # max_list = []
-        # max_val = 0
-        # for key, value in new_dict.items():
-        #     for counter, term in enumerate(value):
-        #         if counter == 0:
-        #             if term >= max_val:
-        #                 if term > max_val and len(max_list) > 0:
-        #                     del max_list[:len(max_list)]
-        #                 max_val = term
-        #                 if max_val > 0:
-        #                     max_list.append((value[1], key))
-
-        # max_list.sort()
 
         # set up im_edit as which filter the user chose
         im_edit = self.my_combo_box.currentText()
 
-        im = Image.open(f'hw3_images/{max_list[0][1]}.jpg')
-        # Options are: ["Pick a value", "Sepia", "Negative", "Grayscale", "Thumbnail", "None"]
-        if (im_edit == "Sepia"):
+        ##TODO change to correct image
+        input_image = Image.open("image.png")
+        input_pixels = input_image.load()
+
+        output_image = Image.new("RGB", input_image.size)
+        draw = ImageDraw.Draw(output_image)
+
+        # Options are: ["Pick a value", "Luminosity", "Contrast", "Colorize", "Sepia", "Negative", "Grayscale", "None"]
+        if (im_edit == "Luminosity"):
+            luminosity = 80
+
+            # Generate image
+            for x in range(output_image.width):
+                for y in range(output_image.height):
+                    r, g, b, a = input_pixels[x, y]
+                    r = int(r + luminosity)
+                    g = int(g + luminosity)
+                    b = int(b + luminosity)
+                    draw.point((x, y), (r, g, b))
+
+            #output_image.save("output.png")
+            output_image.show()
+
+        elif (im_edit == "Contrast"):
+            # Find minimum and maximum luminosity
+            imin = 255
+            imax = 0
+            for x in range(input_image.width):
+                for y in range(input_image.height):
+                    r, g, b, a = input_pixels[x, y]
+                    i = (r + g + b) / 3
+                    imin = min(imin, i)
+                    imax = max(imax, i)
+
+            # Generate image
+            for x in range(output_image.width):
+                for y in range(output_image.height):
+                    r, g, b, a = input_pixels[x, y]
+                    # Current luminosity
+                    i = (r + g + b) / 3
+                    # New luminosity
+                    ip = 255 * (i - imin) / (imax - imin)
+                    r = int(r * ip / i)
+                    g = int(g * ip / i)
+                    b = int(b * ip / i)
+                    draw.point((x, y), (r, g, b))
+
+            output_image.show()
+        elif (im_edit == "Colorize"):
+            # Square distance between 2 colors
+            def distance2(color1, color2):
+                r1, g1, b1 = color1
+                r2, g2, b2, a2 = color2
+                return (r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2
+
+            color_to_change = (0, 0, 255)
+            threshold = 220
+
+            # Generate image
+            for x in range(output_image.width):
+                for y in range(output_image.height):
+                    r, g, b, a = input_pixels[x, y]
+                    if distance2(color_to_change, input_pixels[x, y]) < threshold ** 2:
+                        r = int(r * .5)
+                        g = int(g * 1.25)
+                        b = int(b * .5)
+                    draw.point((x, y), (r, g, b))
+
+            #output_image.save("output.png")
+            output_image.show()
+        elif (im_edit == "Sepia"):
             def sepia(pixel):
                 if pixel[0] < 63:
                     r,g,b = int(pixel[0]*1.1), pixel[1], int(pixel[2]*.9)
@@ -85,33 +133,24 @@ class ImageManipulation(QWidget):
                     if r>255: r=255
                     g,b = pixel[1], pixel[2]//2  
                 return r,g,b
-            sepia_list = map(sepia, im.getdata())
-            im.putdata(list(sepia_list))
-            im.show()
+            sepia_list = map(sepia, input_image.getdata())
+            output_image.putdata(list(sepia_list))
+            #output_image.save("output.png")
+            output_image.show()
         elif (im_edit == "Negative"):
             negative_list = [(255-p[0], 255-p[1], 255-p[2])
-                                for p in im.getdata()]
-            im.putdata(negative_list)
-            im.show()
+                                for p in input_image.getdata()]
+            output_image.putdata(negative_list)
+            #output_image.save("output.png")
+            output_image.show()
         elif (im_edit == "Grayscale"):
             new_list = [ ( (a[0]+a[1]+a[2])//3, ) * 3
-                                for a in im.getdata() ]
-            im.putdata(new_list)
-            im.show()
-        elif (im_edit == "Thumbnail"):
-            def shrinker(your_image, shrink_factor):
-                src = Image.open(your_image)
-                w,h = src.width, src.height
-                trg = Image.new('RGB', ((w//shrink_factor)+1,(h//shrink_factor)+1))
-                for trg_x, src_x in enumerate(range(0, w, shrink_factor)):
-                    for trg_y, src_y in enumerate(range(0, h, shrink_factor)):
-                        px = src.getpixel((src_x, src_y))
-                        trg.putpixel((trg_x, trg_y), px)
-                trg.show()
-
-            shrinker(f'hw3_images/{max_list[0][1]}.jpg', 2)
+                                for a in input_image.getdata() ]
+            output_image.putdata(new_list)
+            #output_image.save("output.png")
+            output_image.show()
         else: #"Pick a value" or "None"
-            im.show()
+            input_image.show()
 
 
     @Slot()
