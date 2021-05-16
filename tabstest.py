202c1d6043d65deb4ad1200f09657c1285ce8771
Ctodd-CSUMB <ctodd@csumb.edu>
2021-05-15 18:05:09 -0700

    Fixed issue with colorize edit
    colorize edit wasn't working when it included a2 when creating r2,g2,b2 variables in def distance2(color1, color2)
    Removing a2 seemed to fix it

--- tabstest.py
+++ tabstest.py
@@ -453,7 +453,7 @@ class ImageManipulation(QWidget):
             # Square distance between 2 colors
             def distance2(color1, color2):
                 r1, g1, b1 = color1
-                r2, g2, b2, a2 = color2
+                r2, g2, b2 = color2
                 return (r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2
 
             color_to_change = (0, 0, 255)
